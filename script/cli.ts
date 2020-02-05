4fd74cb2c6c43b23e5301b18abdb159d69d745fb
Jessica Kerr <jessitron@gmail.com>
2020-02-05 15:06:33 -0800

    Print usage for two few arguments
    But, the bin thing doesn't work. Need to google that.

--- script/cli.ts
+++ script/cli.ts
@@ -6,6 +6,12 @@
 
 import * as mg from "../index"
 
+const usage = `mg <microgrammar string> <input file> [file...]`
+
+if (process.argv.length < 3) {
+    console.log(usage)
+}
+
 const [nodeExecutable, thisScript, mgString, ...files] = process.argv
 
 console.log(`Microgrammar: [${mgString}]`);
